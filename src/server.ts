6dc1643a264a9372cccf591cca2f8f47287f039b
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2026-08-23 16:53:31 +0530

    fix: inline GraphQL SDL for Vercel serverless bundling compatibility

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { createServer } from "node:http";
-import { readFileSync } from "node:fs";
-import { join } from "node:path";
+import { typeDefs } from "./schema/sdl";
 import { resolvers } from "./resolvers";
 import { createContext, GraphQLContext } from "./context";
-
-const typeDefs = readFileSync(join(__dirname, "schema", "schema.graphql"), "utf-8");
 
 export const schema = createSchema<GraphQLContext>({
   typeDefs,
